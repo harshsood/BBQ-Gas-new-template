f9190e04922ed8d540d73a4c3dcc99d5880b648d
Harsh Sood <76679188+harshsood@users.noreply.github.com>
2025-09-24 12:00:14 +0530

    Update Certifications.tsx

--- src/components/Certifications.tsx
+++ src/components/Certifications.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Shield, Award, CheckCircle, Phone } from 'lucide-react';
-import docCert1 from "./images/certification icon with since 1971 text.jpg";
-import docCert2 from "./images/certification icon with since 1975 text.jpg";
+import docCert1 from "../images/certification icon with since 1971 text.jpg";
+import docCert2 from "../images/certification icon with since 1975 text.jpg";
 
 const Certifications = () => {
   return (

70b483ffbc611c5f0f670905d002734185c7844e
Harsh Sood <76679188+harshsood@users.noreply.github.com>
2025-09-24 11:20:50 +0530

    Add files via upload

---
+++ src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Hero from '../components/Hero';
+import Services from '../components/Services';
+import DeliveryAreas from '../components/DeliveryAreas';
+import Certifications from '../components/Certifications';
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <Services />
+      <Certifications />
+      <DeliveryAreas />
+    </>
+  );
+};
+
+export default Home;
